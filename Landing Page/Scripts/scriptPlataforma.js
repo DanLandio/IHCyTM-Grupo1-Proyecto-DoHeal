3a05d9a1428fb1e8fda290e8638180e0e46f23c6
Danilo <danilolan132@gmail.com>
2026-06-18 12:20:19 -0500

    release: Enlazar el indice del landing page con la plataforma -Corregir estilos en landing page y plataforma

--- Landing Page/Scripts/scriptPlataforma.js
+++ Landing Page/Scripts/scriptPlataforma.js
@@ -2,3 +2,11 @@ function abreMenuHamburguesa() {
     document.getElementById("menu-hamburguesa").classList.toggle("active");
     document.getElementById("boton-hamburguesa").classList.toggle("active");
 }
+
+document.addEventListener("DOMContentLoaded", () =>{
+    const logoBoton = document.getElementById("logo-doheal");
+
+    logoBoton.addEventListener("click", () => {
+        document.location.href = "../Estructuras/index.html"
+    })
+})
